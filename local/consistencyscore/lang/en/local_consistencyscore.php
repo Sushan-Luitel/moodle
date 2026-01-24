3c0cd50677ccb426cdf43520916fecc9624e611a
Sushan-Luitel <sushanluitel445@gmail.com>
2026-01-24 21:09:07 +0545

    final thing

--- local/consistencyscore/lang/en/local_consistencyscore.php
+++
@@ -1,3 +0,0 @@
-<?php
-$string['pluginname'] = 'Learning Consistency Score';
-$string['erroraccessdenied'] = 'You are not allowed to view this page';
--- local/consistencyscore/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'local_consistencyscore';
-$plugin->version   = 2026010100;
-$plugin->requires  = 2021051702;
